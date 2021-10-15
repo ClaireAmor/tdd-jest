5f5d11b19198d839647276923a740ebb860be4a9
Claire AMOR <claireamor7033@gmail.com>
2021-10-15 11:13:00 +0200

    ahout de fonction et de test unitaire

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,9 @@
 // Implement the sum function here
+function sum(firstNumber, secondNumber) {
+    if (Number.isInteger(firstNumber) && Number.isInteger(secondNumber)) {
+        return firstNumber + secondNumber;
+    }
+    return false;
+}
+
+module.exports = sum;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,1 +1,14 @@
 // Add the unit tests of the sum function here
+const sum = require('../src/sum');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('adds bonjour + 2 to equal false', () => {
+    expect(sum("bonjour", 2)).toBe(false);
+});
+
+test('adds 1 + bonjoir  to equal Ce nest pas des nombres', () => {
+    expect(sum(1, "bonjoir")).toBe(false);
+});
